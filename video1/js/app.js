3bb95a791d859aafd80b49fe578ea0c3fb1cb9db
Djuan <djuanellis@att.net>
2018-02-06 19:55:07 -0500

    spoiler project

--- video1/js/app.js
+++ video1/js/app.js
@@ -1,5 +1,9 @@
-
-
-
-
-
+//Hide the spoiler text
+$('.spoiler span').hide();
+//When the button is pressed
+$('.spoiler button').click(function(){
+  //Show the spoiler text
+  $('.spoiler span').show();
+  //hid the reveal spoiler button
+  $('.spoiler button').hide();
+})
